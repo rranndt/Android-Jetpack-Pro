25d7a9f089a89e773eea9515938e4de7def2119d
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-31 00:22:57 +0700

    Fix MainActivity.kt

--- 01-MyUnitTesting/app/src/main/java/com/kotlin/myunittesting/MainActivity.kt
+++ 01-MyUnitTesting/app/src/main/java/com/kotlin/myunittesting/MainActivity.kt
@@ -2,10 +2,90 @@ package com.kotlin.myunittesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import com.kotlin.myunittesting.databinding.ActivityMainBinding
+import com.kotlin.myunittesting.model.CuboidModel
+import com.kotlin.myunittesting.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel = MainViewModel(CuboidModel())
+
+        binding.btnSave.setOnClickListener(this)
+        binding.btnCalculateSurfaceArea.setOnClickListener(this)
+        binding.btnCalculateCircumeference.setOnClickListener(this)
+        binding.btnCalculateVolume.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(v: View) {
+        val length = binding.edtLength.text.toString().trim()
+        val width = binding.edtWidth.text.toString().trim()
+        val height = binding.edtHeight.text.toString().trim()
+
+        when {
+            TextUtils.isEmpty(length) -> {
+                binding.edtLength.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(width) -> {
+                binding.edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(height) -> {
+                binding.edtHeight.error = "Field ini tidak boleh kosong"
+            }
+            else -> {
+                val valuelength = length.toDouble()
+                val valueWidth = width.toDouble()
+                val valueHeight = height.toDouble()
+
+                when (v.id) {
+                    R.id.btn_save -> {
+                        mainViewModel.save(valuelength, valueWidth, valueHeight)
+                        visible()
+                    }
+                    R.id.btn_calculate_circumeference -> {
+                        binding.tvResult.text = mainViewModel.getCircumference().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_surface_area -> {
+                        binding.tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_volume -> {
+                        binding.tvResult.text = mainViewModel.getVolume().toString()
+                        gone()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun visible() {
+        binding.btnCalculateVolume.visibility = View.VISIBLE
+        binding.btnCalculateCircumeference.visibility = View.VISIBLE
+        binding.btnCalculateSurfaceArea.visibility = View.VISIBLE
+        binding.btnSave.visibility = View.GONE
+    }
+
+    private fun gone() {
+        binding.btnCalculateVolume.visibility = View.GONE
+        binding.btnCalculateCircumeference.visibility = View.GONE
+        binding.btnCalculateSurfaceArea.visibility = View.GONE
+        binding.btnSave.visibility = View.VISIBLE
     }
 }
