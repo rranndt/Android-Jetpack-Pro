a4ddb44b087ed1436b3ac2dcf5acee33eec2c550
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-31 22:10:53 +0700

    Add Fix MainActivity.kt

--- 02-MyViewModel/app/src/main/java/com/kotlin/myviewmodel/MainActivity.kt
+++ 02-MyViewModel/app/src/main/java/com/kotlin/myviewmodel/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.kotlin.myviewmodel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.kotlin.myviewmodel.databinding.ActivityMainBinding
+import com.kotlin.myviewmodel.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[MainViewModel::class.java]
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
