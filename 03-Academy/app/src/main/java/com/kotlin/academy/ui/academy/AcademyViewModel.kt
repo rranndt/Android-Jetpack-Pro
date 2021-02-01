93ae9f721d7bae6784caeb01e8e578f3d8aac948
AnanditaRian19 <rizkianandita6@gmail.com>
2021-02-02 00:06:47 +0700

    Fix AcademyViewModel.kt

--- 03-Academy/app/src/main/java/com/kotlin/academy/ui/academy/AcademyViewModel.kt
+++ 03-Academy/app/src/main/java/com/kotlin/academy/ui/academy/AcademyViewModel.kt
@@ -1,8 +1,15 @@
 package com.kotlin.academy.ui.academy
 
+import androidx.lifecycle.ViewModel
+import com.kotlin.academy.data.CourseEntity
+import com.kotlin.academy.utils.DataDummy
+
 /**
  *@author Rizki Rian Anandita
  * Create By rizki
  */
-class AcademyViewModel {
+class AcademyViewModel : ViewModel() {
+
+    fun getCourses(): List<CourseEntity> = DataDummy.generateDummyCourse()
+
 }
