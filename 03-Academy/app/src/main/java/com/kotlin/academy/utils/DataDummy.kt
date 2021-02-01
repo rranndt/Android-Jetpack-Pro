6debf62174328bddcb86e907f4e601cbc8544259
AnanditaRian19 <rizkianandita6@gmail.com>
2021-02-02 00:37:58 +0700

    Fix DataDummy.kt

--- 03-Academy/app/src/main/java/com/kotlin/academy/utils/DataDummy.kt
+++ 03-Academy/app/src/main/java/com/kotlin/academy/utils/DataDummy.kt
@@ -66,7 +66,7 @@ object DataDummy {
         return courses
     }
 
-    fun generateDummyModules(courseId: String): List<ModuleEntity> {
+    fun generateDummyModules(courseId: String): ArrayList<ModuleEntity> {
         val modules = ArrayList<ModuleEntity>()
 
         modules.add(
