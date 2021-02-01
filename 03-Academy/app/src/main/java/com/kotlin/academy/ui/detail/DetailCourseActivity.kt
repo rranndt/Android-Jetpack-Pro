8c90c55d5ceccc3a86596b840e8b6bcf309fd4e3
AnanditaRian19 <rizkianandita6@gmail.com>
2021-02-02 00:17:30 +0700

    Update to View Model

--- 03-Academy/app/src/main/java/com/kotlin/academy/ui/detail/DetailCourseActivity.kt
+++ 03-Academy/app/src/main/java/com/kotlin/academy/ui/detail/DetailCourseActivity.kt
@@ -3,6 +3,7 @@ package com.kotlin.academy.ui.detail
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -34,17 +35,25 @@ class DetailCourseActivity : AppCompatActivity() {
 
         val adapter = DetailCourseAdapter()
 
+        val viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[DetailCourseViewModel::class.java]
+
         val extras = intent.extras
         if (extras != null) {
             val courseId = extras.getString(EXTRA_COURSE)
             if (courseId != null) {
-                val modules = DataDummy.generateDummyModules(courseId)
+                viewModel.setSelectedCourse(courseId)
+//                val modules = DataDummy.generateDummyModules(courseId)
+                val modules = viewModel.getModule()
                 adapter.setModules(modules)
-                for (course in DataDummy.generateDummyCourse()) {
-                    if (course.courseId == courseId) {
-                        populateCourse(course)
-                    }
-                }
+//                for (course in DataDummy.generateDummyCourse()) {
+//                    if (course.courseId == courseId) {
+//                        populateCourse(course)
+//                    }
+//                }
+                populateCourse(viewModel.getCourse() as CourseEntity)
             }
         }
 
