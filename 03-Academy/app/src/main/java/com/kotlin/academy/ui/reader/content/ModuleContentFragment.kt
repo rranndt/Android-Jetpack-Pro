1077a4cb9667e5de2729321e53330f0fa9914fa2
AnanditaRian19 <rizkianandita6@gmail.com>
2021-02-02 00:38:17 +0700

    Update to View Model

--- 03-Academy/app/src/main/java/com/kotlin/academy/ui/reader/content/ModuleContentFragment.kt
+++ 03-Academy/app/src/main/java/com/kotlin/academy/ui/reader/content/ModuleContentFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.kotlin.academy.data.ContentEntity
+import com.kotlin.academy.data.ModuleEntity
 import com.kotlin.academy.databinding.FragmentModuleContentBinding
+import com.kotlin.academy.ui.reader.CourseReaderViewModel
 
 class ModuleContentFragment : Fragment() {
 
@@ -30,14 +34,24 @@ class ModuleContentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (activity != null) {
-            val content =
-                ContentEntity("<h3 class=\\\"fr-text-bordered\\\">Contoh Content</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
-            populateWebView(content)
+            val viewModel = ViewModelProvider(
+                requireActivity(),
+                ViewModelProvider.NewInstanceFactory()
+            )[CourseReaderViewModel::class.java]
+//            val content =
+//                ContentEntity("<h3 class=\\\"fr-text-bordered\\\">Contoh Content</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
+//            populateWebView(content)
+            val module = viewModel.getSelectedModule()
+            populateWebView(module)
         }
     }
 
-    private fun populateWebView(content: ContentEntity) {
-        binding.webView.loadData(content.content ?: "", "text/html", "UTF-8")
+//    private fun populateWebView(content: ContentEntity) {
+//        binding.webView.loadData(content.content ?: "", "text/html", "UTF-8")
+//    }
+
+    private fun populateWebView(module: ModuleEntity) {
+        binding.webView.loadData(module.contentEntity?.content ?: "", "text/html", "UTF-8")
     }
 
     override fun onDestroy() {
