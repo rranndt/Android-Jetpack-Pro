3443d06cbc7d465e84971de24b82a2682097048a
AnanditaRian19 <rizkianandita6@gmail.com>
2021-02-02 22:42:13 +0700

    Fix DetailCourseViewModelTest.kt

---
+++ 03-Academy/app/src/test/java/com/kotlin/academy/ui/detail/DetailCourseViewModelTest.kt
@@ -0,0 +1,43 @@
+package com.kotlin.academy.ui.detail
+
+import com.kotlin.academy.utils.DataDummy
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * @author Rizki Rian Anandita
+ * Create By rizki
+ */
+class DetailCourseViewModelTest {
+
+    private lateinit var viewModel: DetailCourseViewModel
+    private val dummyCourse = DataDummy.generateDummyCourse()[0]
+    private val courseId = dummyCourse.courseId
+
+    @Before
+    fun setUp() {
+        viewModel = DetailCourseViewModel()
+        viewModel.setSelectedCourse(courseId)
+    }
+
+    @Test
+    fun getCourse() {
+        viewModel.setSelectedCourse(dummyCourse.courseId)
+        val courseEntity = viewModel.getCourse()
+        assertNotNull(courseEntity)
+        assertEquals(dummyCourse.courseId, courseEntity.courseId)
+        assertEquals(dummyCourse.deadline, courseEntity.deadline)
+        assertEquals(dummyCourse.description, courseEntity.description)
+        assertEquals(dummyCourse.imagePath, courseEntity.imagePath)
+        assertEquals(dummyCourse.title, courseEntity.title)
+    }
+
+    @Test
+    fun getModule() {
+        val moduleEntities = viewModel.getModule()
+        assertNotNull(moduleEntities)
+        assertEquals(7, moduleEntities.size.toLong())
+    }
+}
