2996fb17467aac2218fb3080b8211ae13d3e69cc
AnanditaRian19 <rizkianandita6@gmail.com>
2021-02-09 23:48:00 +0700

    fix update

--- 06-MyLiveData/app/src/main/java/com/kotlin/mylivedata/MainActivity.kt
+++ 06-MyLiveData/app/src/main/java/com/kotlin/mylivedata/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.kotlin.mylivedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.kotlin.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mLiveDataTimerViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            binding.timerTextview.text = newText
+        }
+
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
---
+++ 06-MyLiveData/app/src/main/java/com/kotlin/mylivedata/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.kotlin.mylivedata
+
+import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+/**
+ *@author Rizki Rian Anandita
+ * Create By rizki
+ */
+class MainViewModel : ViewModel() {
+
+    companion object {
+        private const val ONE_SECOND = 1000
+    }
+    private val mInitialTime = SystemClock.elapsedRealtime()
+    private val mElapsedTime = MutableLiveData<Long?>()
+
+    init {
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+                mElapsedTime.postValue(newValue)
+            }
+        }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
+
+    fun getElapsedTime() : LiveData<Long?> {
+        return mElapsedTime
+    }
+
+}
