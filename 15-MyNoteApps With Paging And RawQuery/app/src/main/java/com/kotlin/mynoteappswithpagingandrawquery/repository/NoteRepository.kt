9799cf1b5d29bfb676f559ccc29e77aae2b28ac8
AnanditaRian19 <rizkianandita6@gmail.com>
2021-03-07 00:55:42 +0700

    add property and variable in getAllNotes

--- 15-MyNoteApps With Paging And RawQuery/app/src/main/java/com/kotlin/mynoteappswithpagingandrawquery/repository/NoteRepository.kt
+++ 15-MyNoteApps With Paging And RawQuery/app/src/main/java/com/kotlin/mynoteappswithpagingandrawquery/repository/NoteRepository.kt
@@ -5,6 +5,7 @@ import androidx.paging.DataSource
 import com.kotlin.mynoteappswithpagingandrawquery.database.Note
 import com.kotlin.mynoteappswithpagingandrawquery.database.NoteDao
 import com.kotlin.mynoteappswithpagingandrawquery.database.NoteRoomDatabase
+import com.kotlin.mynoteappswithpagingandrawquery.helper.SortUtils
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -22,8 +23,9 @@ class NoteRepository(application: Application) {
         mNotesDao = db.noteDao()
     }
 
-    fun getAllNotes(): DataSource.Factory<Int, Note> {
-        return mNotesDao.getAllNotes()
+    fun getAllNotes(sort: String): DataSource.Factory<Int, Note> {
+        val query = SortUtils.getSortedQuery(sort)
+        return mNotesDao.getAllNotes(query)
     }
 
     fun insert(note: Note) {
